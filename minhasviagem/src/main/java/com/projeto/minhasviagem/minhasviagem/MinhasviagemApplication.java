0dfa96b899a79ab094fd1425056a15d775b26af3
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-07-12 01:11:38 -0300

    configuração inicial

---
+++ minhasviagem/src/main/java/com/projeto/minhasviagem/minhasviagem/MinhasviagemApplication.java
@@ -0,0 +1,14 @@
+package com.projeto.minhasviagem.minhasviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasviagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasviagemApplication.class, args);
+		
+	}
+
+}
---
+++ minhasviagem/src/test/java/com/projeto/minhasviagem/minhasviagem/MinhasviagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.minhasviagem.minhasviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasviagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
